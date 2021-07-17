bdd54a35e2f1261229683c58ebfd4b2ca81198b5
Ricardo David <ricardo.david2020.new@gmail.com>
2021-07-17 22:18:33 +0100

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdeliveree/DsdelivereeApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliveree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdelivereeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdelivereeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsdeliveree/DsdelivereeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliveree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdelivereeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
